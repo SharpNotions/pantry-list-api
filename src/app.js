6f1cb81fb7e048ca90ed5b05514721a9a22ad077
Travis Stodter <travis.stodter@sharpnotions.com>
2018-03-15 23:30:08 -0400

    Add koa-bodyparser

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const Koa = require('koa');
 const Router = require('koa-router');
 const logger = require('koa-logger');
+const bodyParser = require('koa-bodyparser');
 
 const app = new Koa();
 const router = new Router();
