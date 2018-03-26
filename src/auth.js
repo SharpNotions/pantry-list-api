c7cfc04f762002093e0310ba0b5562b0c918c30a
Travis Stodter <travis.stodter@sharpnotions.com>
2018-03-26 12:27:42 -0400

    Add Users table & model

--- src/auth.js
+++ src/auth.js
@@ -4,6 +4,24 @@ const CLIENT_ID =
   '270040816063-djog7f8mpvt4m162ak4n04bjmftg1bhc.apps.googleusercontent.com';
 const client = new OAuth2Client(CLIENT_ID);
 
+const getUser = async (ctx, payload) => {
+  let [user] = await ctx.app.models.User.query()
+    .where('auth_id', payload.sub);
+
+  if (!user) {
+    user = await ctx.app.models.User.query()
+      .insert({
+        auth_id   : payload.sub,
+        email     : payload.email,
+        first_name: payload.given_name,
+        last_name : payload.family_name
+      })
+      .returning('*');
+  }
+
+  return user;
+};
+
 const requireAuth = async (ctx, next) => {
   ctx.assert(ctx.header.authorization, 401, 'No auth header found.');
   const [_, idToken] = ctx.header.authorization.split(' ');
@@ -16,6 +34,8 @@ const requireAuth = async (ctx, next) => {
     });
     const payload = ticket.getPayload();
     ctx.assert(payload.hd === 'sharpnotions.com', 401, 'Not authorized');
+
+    ctx.state.user = await getUser(ctx, payload);
 
     await next();
   } catch (err) {
--- src/db/db-middleware.js
+++ src/db/db-middleware.js
@@ -1,12 +1,40 @@
 const db = require('./connection');
-const { Model } = require('objection');
+const objection = require('objection');
 const models = require('../models')
 
-module.exports = (app) => {
-  Model.knex(db);
-  app.models = models;
-  return function (ctx, next) {
-    return next()
+class SoftDeleteQueryBuilder extends objection.QueryBuilder {
+  constructor(modelClass) {
+    super(modelClass)
+
+    this.onBuild(query => {
+      if (query.isFind() && !query.context().includeDeleted) {
+        query.whereNull(`${modelClass.tableName}.deleted_at`);
+      }
+    });
   }
 
+  includeDeleted() {
+    return this.context({
+      includeDeleted: true
+    });
+  }
+
+  delete(...args) {
+    return super.patch({ deleted_at: new Date() });
+  }
+
+  forceDelete(...args) {
+    return super.delete(...args);
+  }
+}
+
+module.exports = (app) => {
+  objection.QueryBuilder = SoftDeleteQueryBuilder;
+  objection.Model.QueryBuilder = SoftDeleteQueryBuilder;
+  objection.Model.RelatedQueryBuilder = SoftDeleteQueryBuilder;
+
+  objection.Model.knex(db);
+  app.models = models;
+
+  return (ctx, next) => next();
 }
