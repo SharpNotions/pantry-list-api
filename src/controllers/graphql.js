769af00489ab4be7eb85df49e6ddc7b82fe4d9a3
Phill Ayers <phillip.ayers@sharpnotions.com>
2018-03-29 10:39:34 -0400

    feat: add user model to graphql schema

--- src/controllers/graphql.js
+++ src/controllers/graphql.js
@@ -1,8 +1,10 @@
 const { graphqlKoa, graphiqlKoa } = require('apollo-server-koa');
 const { builder } = require('objection-graphql');
 const Item = require('../models/Item');
+const User = require('../models/User');
 const UserRanking = require('../models/UserRanking')
 const schema = builder()
+  .model(User, {listFieldName: 'users', fieldName: 'user'})
   .model(Item, {listFieldName: 'items', fieldName: 'item'})
   .model(UserRanking, {listFieldName: 'userRankings', fieldName: 'userRanking'})
   .build();
