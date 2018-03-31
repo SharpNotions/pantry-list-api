c4745bc6bd4f6973244e254f82aa29a8857d62c0
Phill Ayers <phillip.ayers@sharpnotions.com>
2018-03-31 08:41:35 -0400

    feat: Recursively generate query

--- src/controllers/rankings.js
+++ src/controllers/rankings.js
@@ -1,57 +1,42 @@
-const { graphql } = require('graphql');
-const { schema } = require('./graphql')
+const { graphql } = require('graphql')
+const { schema }  = require('./graphql')
+const { pathOr }  = require('ramda');
 
-const query = `
-query UserRankings($user_id: Int) {
-  userRankings(prev_ranking_idIsNull: true, user_id: $user_id) {
-    ...RankingFields
-    ...RankingsRecursive
-  }
-}
-
-fragment RankingsRecursive on UserRankings {
-  nextRanking {
-    ...RankingFields
-    nextRanking {
-      ...RankingFields
-      nextRanking {
+const query = (depth) => {
+  const recursiveRankings = generateRankingsRecursive(depth)
+  return `
+    query UserRankings($user_id: Int) {
+      userRankings(prev_ranking_idIsNull: true, user_id: $user_id) {
         ...RankingFields
-        nextRanking {
-          ...RankingFields
-          nextRanking {
-            ...RankingFields
-            nextRanking {
-              ...RankingFields
-              nextRanking {
-                ...RankingFields
-                nextRanking {
-                  ...RankingFields
-                }
-              }
-            }
-          }
+        ${recursiveRankings}
+      }
+    }
+
+    fragment RankingFields on UserRankings {
+      ranking_id: id
+      item {
+        id
+        item_name
+        item_details {
+          description
         }
       }
     }
-  }
+  `
 }
-
-fragment RankingFields on UserRankings {
-  ranking_id: id
-  item {
-    id
-    item_name
-    item_details {
-      description
-    }
-  }
-}
-`
 
 async function getUserRankings(ctx, next) {
-  const { data } = await graphql(schema, query, null, null, {user_id: ctx.state.user.id})
+  const depth = pathOr(10, ['request', 'query', 'depth'], ctx)
+  const { data } = await graphql(schema, query(depth), null, null, {user_id: 1})
   const flattenedUserRankings = flattenRankings(data.userRankings[0], [])
   ctx.body = flattenedUserRankings
+}
+
+function generateRankingsRecursive(n) {
+  if (n === 1) {
+    return 'nextRanking { ...RankingFields }'
+  }
+  return `nextRanking { ...RankingFields ${generateRankingsRecursive(n - 1)}}`
 }
 
 function flattenRankings(currentItem, rankings) {
