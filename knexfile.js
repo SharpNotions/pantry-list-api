d30b79403000ba6dc74032df686d7af73811c442
Adam Macumber <adam.macumber@sharpnotions.com>
2018-05-27 17:30:24 +0000

    feat: Use DATABASE_URL if available, otherwise use separate vars.

--- knexfile.js
+++ knexfile.js
@@ -2,22 +2,23 @@ const path = require('path');
 const baseConfig = {
   user: process.env.DATABASE_USER || 'pantry_user',
   password: process.env.DATABASE_PASS || 'pantry_pass',
-  host: process.env.DATABASE_URL || 'pantry-list-api_pg_1',
+  host: process.env.DATABASE_HOST || 'localhost',
+  port: process.env.DATABASE_PORT || '5432',
   database: process.env.DATABASE_NAME || 'pantry'
 }
 
 // Build a connection string from a given configuration
-const getConnectionString = ({ user, password, host, database }) =>
-  `postgres://${user}:${password}@${host}/${database}`
+const getConnectionString = ({ user, password, host, database, port }) =>
+  `postgres://${user}:${password}@${host}:${port}/${database}`
 
 const config = (nodeEnv) => ({
   client: 'pg',
   connection: {
-    test       : getConnectionString({
+    test       : process.env.DATABASE_URL || getConnectionString({
       ...baseConfig,
       database: 'pantry_test'
     }),
-    development: getConnectionString(baseConfig),
+    development: process.env.DATABASE_URL || getConnectionString(baseConfig),
     // Production connection string is managed by Heroku
     production : process.env.DATABASE_URL
   }[nodeEnv],
