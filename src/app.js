49079319c16c69ce767374bfffbe05cfaa5adf76
Phill Ayers <phillip.ayers@sharpnotions.com>
2018-06-09 17:06:22 -0400

    feat: Add ability to auth with google without frontend

--- src/app.js
+++ src/app.js
@@ -2,9 +2,13 @@ const Koa        = require('koa');
 const Router     = require('koa-router');
 const logger     = require('koa-logger');
 const bodyParser = require('koa-bodyparser');
+const mount      = require('koa-mount')
+const grant      = require('grant-koa')
+const session    = require('koa-session')
 const auth       = require('./auth');
 const R          = require('ramda');
 const db         = require('./db/db-middleware');
+const config     = require('./config');
 const {
   getItems, addItem
 } = require('./controllers/items');
@@ -19,6 +23,11 @@ const router = new Router();
 router
   .get('/', (ctx, next) => {
     ctx.body = 'Hello World';
+  })
+  .get('/handle_google_callback', (ctx, next) => {
+    ctx.assert(ctx.session.grant.response.raw.id_token, 401, 'Auth Failed')
+    ctx.cookies.set('id_token', ctx.session.grant.response.raw.id_token);
+    ctx.redirect(ctx.cookies.get('after_login'));
   })
   .get('*/ping', (ctx, next) => {
     ctx.body = `pong ${new Date().toString()}`;
@@ -37,9 +46,12 @@ router
   .get('*/top_rankings', getTopRankings)
   .get('*/buildusers', createUsers)
 
+app.keys = ['grant'];
 app.use(db(app));
 app.use(logger());
 app.use(bodyParser());
+app.use(session(app));
+app.use(mount(grant(config[process.env.NODE_ENV || 'development'])));
 app.use(router.routes());
 app.use(router.allowedMethods());
 
